92b3fbb0ee0442e45e5b2292964c099a107ead2a
Bruno Amorim <bruno_amorim1@estudante.sc.senai.br>
2019-07-04 21:21:32 -0300

    Envio de dados via AJAX

--- index.php
+++ index.php
@@ -223,7 +223,7 @@
     
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <!-- Plugin jQuery Validate -->
@@ -275,6 +275,23 @@
            });
            
        });
+        //Envio de dados via Ajax
+        //Sem recarregar a página
+            $("#btnRegistroUsuario").click(function(e){
+            if(document.querySelector("#formRegistro").checkValidity()){
+                e.preventDefault();
+                $.ajax({
+                    url: 'recebe.php',
+                    method: 'post',
+                    data: $('#formRegistro').serialize()+'&action=registro',
+                    success:function(resposta){
+                        $('#alerta').show();
+                        $('#resultado').html(resposta);
+                        
+                    }
+                });
+            }
+            });
        
        //Tradução do jQuery Validate
        /*
